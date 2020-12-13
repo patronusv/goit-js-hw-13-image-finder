0933fad79672c7d023a01e1d20324011bcba4aa3
Viktor Gorokhov <patronusv@gmail.com>
2020-12-14 01:19:05 +0200

    search function will not reset on empty search query

--- src/index.js
+++ src/index.js
@@ -1,31 +1,43 @@
 import './styles.css';
+import debounce from 'lodash/debounce.js';
 import { refs } from './refs/refs.js';
 import imageService from './api/apiService.js';
 import { updateMarkup, resetMarkup } from './components/updateMarkup.js';
 import { buttonShow, buttonDisable } from './components/buttonLoad.js';
 
 const fetchImages = e => {
-  imageService
-    .fetchImages(e)
-    .then(data => {
-      updateMarkup(data);
-      buttonShow();
-    })
-    .then(() => {
-      window.scrollBy({
-        top: document.documentElement.offsetHeight,
-        behavior: 'smooth',
+  if (!imageService.query) {
+    return;
+  } else
+    imageService
+      .fetchImages(e)
+      .then(data => {
+        updateMarkup(data);
+        buttonShow();
+      })
+      .then(() => {
+        window.scrollBy({
+          top: document.documentElement.offsetHeight,
+          behavior: 'smooth',
+        });
       });
-    });
 };
 const formHandler = e => {
   console.log(e);
-  resetMarkup();
+  const previousQuery = imageService.query;
+  console.log(previousQuery);
   imageService.page = 1;
   imageService.query = e.target.value;
-  fetchImages(e);
+  if (imageService.query !== previousQuery && imageService.query) {
+    resetMarkup();
+    fetchImages(e);
+  }
 };
-refs.searchForm.addEventListener('change', e => formHandler(e));
+// refs.searchForm.addEventListener('change', e => formHandler(e));
+refs.searchForm.addEventListener(
+  'input',
+  debounce(e => formHandler(e), 500),
+);
 refs.searchForm.addEventListener('submit', e => e.preventDefault());
 refs.buttonLoad.addEventListener('click', e => {
   fetchImages(e);

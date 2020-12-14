5cdf4ed9db1e79e378bdf115757f842184304c82
Viktor Gorokhov <patronusv@gmail.com>
2020-12-14 16:51:00 +0200

    adds close modal on esc function

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,7 +1,19 @@
 import * as basicLightbox from 'basiclightbox';
 import { refs } from '../refs/refs.js';
 import 'basiclightbox/dist/basicLightbox.min.css';
-const instance = basicLightbox.create(refs.modalTemplate);
+const onEscPress = e => {
+  if (e.code === 'Escape') {
+    instance.close();
+  }
+};
+const instance = basicLightbox.create(refs.modalTemplate, {
+  onShow() {
+    window.addEventListener('keydown', onEscPress);
+  },
+  onClose() {
+    window.removeEventListener('keydown', onEscPress);
+  },
+});
 export const openModal = e => {
   if (e.target.nodeName !== 'IMG') return;
   instance.show();
